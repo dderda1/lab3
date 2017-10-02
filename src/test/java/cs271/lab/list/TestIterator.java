9bf8b863ae69233c10bb70751917a259a0824ad3
Daria Derda <dderda1@luc.edu>
2017-10-02 23:59:47 +0000

    main portion done

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -96,13 +96,13 @@ public class TestIterator {
     list.add(66);
     double sum = 0;
     int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
+    // DONE use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
     final Iterator<Integer> i = list.iterator();
     while (i.hasNext()) {
-      n = i.next();
-      sum =+ n;
+     sum += i.next();
+      n++;
   }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
